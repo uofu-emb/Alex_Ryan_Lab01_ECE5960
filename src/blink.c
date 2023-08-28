e19953828a77e93e50c5b16924fbb218d306f1ef
Alex <alex@compuguy.dev>
2023-08-28 16:01:17 -0600

    Commented the blink.c code with assumptions of functionality

--- src/blink.c
+++ src/blink.c
@@ -33,18 +33,22 @@ bool led_is_on;
 
 void thread_entry(void)
 {
+    // Get the same values for LED1 as at the beginning of main for LED0
     const struct device *dev;
     dev = device_get_binding(LED1);
     bool led_is_on = true;
 	int ret = gpio_pin_configure(dev, PIN0, GPIO_OUTPUT_ACTIVE | FLAGS0);
 
+    // Set up a k_timer to keep track of time
 	struct k_timer t;
 	k_timer_init(&t, NULL, NULL);
 
+    // Increment a counter every time the loop runs and change the status of the LED.
 	while (1) {
         counter = counter + 1;
 		gpio_pin_set(dev, PIN1, (int)led_is_on);
 		led_is_on = !led_is_on;
+        // Start the k_timer on an interval of 2 seconds. 
 		k_timer_start(&t, K_MSEC(2000), K_NO_WAIT);
 		k_timer_status_sync(&t);
 	}
@@ -52,12 +56,15 @@ void thread_entry(void)
 
 void main(void)
 {
+    // Create a device struct pointer, assume led is on, and create a local int to capture return value of configure.
 	const struct device *dev;
 	led_is_on = true;
 	int ret;
 
+    // set device pointer to memory mapped io location for LED0.
 	dev = device_get_binding(LED0);
 
+    // Create a thread for doing stuff related to a k_thread.
     k_thread_create(&coop_thread,
                     coop_stack,
                     STACKSIZE,
@@ -69,15 +76,18 @@ void main(void)
                     0,
                     K_NO_WAIT);
 
+    // If the LED device wasn't configured correctly, exit early
 	if (dev == NULL) {
 		return;
 	}
 
+    // configure the pin to be output for led setting. If this fails, exit early.
 	ret = gpio_pin_configure(dev, PIN0, GPIO_OUTPUT_ACTIVE | FLAGS0);
 	if (ret < 0) {
 		return;
 	}
 
+    // Alternate the status of the LED every half second.
 	while (1) {
 		gpio_pin_set(dev, PIN0, (int)led_is_on);
 		led_is_on = !led_is_on;
